b08d8726a6b076df76c728e01c9d8faff4866535
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2019-03-16 19:06:04 -0300

    2019-03-16 19:05

--- ABM/App_Start/RouteConfig.cs
+++ ABM/App_Start/RouteConfig.cs
@@ -9,11 +9,10 @@ namespace ABM {
 	public class RouteConfig {
 		public static void RegisterRoutes(RouteCollection routes) {
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Home", action = "Sales", id = UrlParameter.Optional }
 			);
 		}
 	}
