f2367057bda2d5845a520c6d4f1a8651c68aee95
Daniel Barria <daniel.b.a@hotmail.es>
2019-04-18 00:10:52 -0400

    proyecto de datos

---
+++ ABM/Base/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ABM.Datos.SQL;
+
+namespace ABM.Base
+{
+    public class BaseController : Controller
+    {
+        protected ABMEntities BddABM { get; set; }
+        protected BaseController()
+        {
+            BddABM = new ABMEntities();
+        }
+
+        protected TBL_USUARIO Usuario { get { return (TBL_USUARIO)Session["Usuario"] ?? new TBL_USUARIO(); } set { Session["Usuario"] = value; } }
+    }
+}
