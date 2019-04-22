0538da917786e9f5d6ed6f592cb951648f63f9ec
Alan Gárate <CDL.ProDrifter.DK@hotmail.com>
2019-04-22 18:12:08 -0400

    Agregados los sliders en el index e imágenes de ejemplo. Eliminada la vista de BestSellers y el controllador correspondiente.

--- ABM/Controllers/HomeController.cs
+++ ABM/Controllers/HomeController.cs
@@ -7,7 +7,6 @@ using System.Web.Mvc;
 namespace ABM.Controllers {
 	public class HomeController : Controller {
 		public ActionResult Sales() => this.View();
-		public ActionResult BestSellers() => this.View();
 		public ActionResult Product() => this.View();
         public ActionResult Description() => this.View();
     }
